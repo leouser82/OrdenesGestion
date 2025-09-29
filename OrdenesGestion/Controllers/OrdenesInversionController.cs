ce3a322f7dda412e7cc94bdf9109dcaa625c4c84
Leonel.Buedikman@bupa.cl <Leonel.Buedikman@bupa.cl>
2025-09-29 16:15:10 -0300

    prueba 3

--- OrdenesGestion/Controllers/OrdenesInversionController.cs
+++ OrdenesGestion/Controllers/OrdenesInversionController.cs
@@ -47,7 +47,7 @@ namespace GestionOrdenes.Controllers
         public async Task<IActionResult> GetAllOrdenesByTipo(string tipo)
         {
             var ordenes = await _ordenService.GetAllOrdenesAsync();
-            return Ok(ordenes); //ddddd  
+            return Ok(ordenes); //ddddddddss  
         }
 
         /// <summary>
